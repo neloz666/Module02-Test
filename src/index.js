88063f31dbf0d3f2444ccfd6f090c50d0a28cbfd
neloz666 <i@karpov-7.ru>
2023-05-18 16:20:24 +0300

    homework lesson 11

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import 'normalize.css'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss'
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  }); 
